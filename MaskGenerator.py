a577b63b4cfd428a0fdbcc5ab29b2d11d85df7c2
Alex Barbera <alex_barbera.m@hotmail.com>
2023-11-21 15:44:16 -0500

    Added dilation for cytoplasm segmentation

--- MaskGenerator.py
+++ MaskGenerator.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy
 import scipy.ndimage
 import torch
@@ -8,17 +10,19 @@ import tqdm
 
 
 class MaskGenerator:
-    def __init__(self, component_index=2, mask_strategy="addition", gmm_strategy: str = "all"):
+    def __init__(self, component_index=2, mask_strategy="addition", gmm_strategy: str = "all", dilation: int = 3):
         """
         :param component_index: GMM component to use for thresholding 0 (lowest) - 2 (highest).
         :type component_index: int
         :param mask_strategy: How to integrate masks into single file, addition where overlapping masks are added. xor for logical xor for masking where overlapping masks are ignored on intersection.
         :param gmm_strategy: Choose whether to train the gmm with all the tiles (all) or one gmm per cell (independent)
+        :param dilation: Pixels to dilate for cytoplasm inclusion. 0 Or None for no dilation.
         """
         self.component_index = component_index
         self.mask_strategy = mask_strategy
         self.random = numpy.random
         self.gmm_strategy = gmm_strategy
+        self.dilation = dilation
 
     def select_random(self, x, y):
         i = 0
@@ -66,9 +70,9 @@ class MaskGenerator:
         j = i - min(input.shape)
 
         if input.shape[0] < input.shape[1]:
-            output = numpy.vstack( (input, numpy.zeros((j, i))) )
+            output = numpy.vstack((input, numpy.zeros((j, i))))
         else:
-            output = numpy.hstack( (input, numpy.zeros((i, j))) )
+            output = numpy.hstack((input, numpy.zeros((i, j))))
 
         output[0:input.shape[0], 0:input.shape[1]] = input
 
@@ -108,8 +112,7 @@ class MaskGenerator:
             x, y = self.select_random(x, y)
             output = skimage.segmentation.flood(output, (x, y))
 
-            #output = numpy.logical_and(output, mask)
-
+            # output = numpy.logical_and(output, mask)
 
         output = scipy.ndimage.binary_fill_holes(output).astype(int)
 
@@ -118,7 +121,6 @@ class MaskGenerator:
     def _generate_mask_from_scratch(self, cell: torch.tensor):
         threshold = self._calculate_threshold_gmm(cell)
         return self._generate_mask(cell, threshold, None)
-
 
     def generate_list(self, tiff: torch.FloatTensor, boxes: torch.IntTensor | numpy.ndarray):
         """
@@ -172,7 +174,27 @@ class MaskGenerator:
 
         return output
 
-    def generate_mask_output(self, tiff: torch.FloatTensor, boxes: torch.IntTensor | numpy.ndarray, masks: list = None) -> numpy.ndarray:
+    @staticmethod
+    def _internal_xor(final, box, mask):
+        return numpy.logical_xor(final[box[1]:box[3], box[0]:box[2]], mask).astype(int)
+
+    @staticmethod
+    def _internal_new_xor(final, box, mask, i):
+        temp = numpy.logical_xor(final[box[1]:box[3], box[0]:box[2]], mask).astype(int)
+        m1 = final[box[1]:box[3], box[0]:box[2]] * temp
+        m1 += (numpy.logical_xor(m1, mask) * (i + 1))
+
+        return m1
+
+    @staticmethod
+    def _internal_ignore(final, box, mask):
+        m = mask - final[box[1]:box[3], box[0]:box[2]]
+        m[m <= 0] = 0
+
+        return m
+
+    def generate_mask_output(self, tiff: torch.FloatTensor, boxes: torch.IntTensor | numpy.ndarray,
+                             masks: list = None) -> numpy.ndarray:
         """
         Generates a \code{numpy.ndarray} with the same dimensions as \code{tiff} but with each cell detected by \code{boxes} masked by a GMM with 3 components.
         Each cell is assigned a unique sequential integer value.
@@ -181,35 +203,63 @@ class MaskGenerator:
         :param masks: (Optional) Masks from the prediction, thresholded to binary.
         """
         output = numpy.zeros_like(tiff)
+        output_dilated = None
+        dilation_extra = 0
+
+        if self.dilation is not None or self.dilation != 0:
+            output_dilated = numpy.zeros_like(tiff)
+            dilation_extra = math.ceil(self.dilation / 2)
 
         if self.gmm_strategy == "all":
             threshold = self._calculate_threshold_from_tiff(tiff, boxes)
 
             for i, box in tqdm.tqdm(enumerate(boxes), desc="Creating masks with threshold", total=boxes.shape[0]):
                 m = self._generate_mask(
-                        torchvision.transforms.functional.crop(
-                            tiff,
-                            box[1],
-                            box[0],
-                            box[3] - box[1],
-                            box[2] - box[0]
-                        ),
-                        threshold,
-                        masks[i] if masks is not None else None
-                    )
+                    torchvision.transforms.functional.crop(
+                        tiff,
+                        box[1],
+                        box[0],
+                        box[3] - box[1],
+                        box[2] - box[0]
+                    ),
+                    threshold,
+                    masks[i] if masks is not None else None
+                )
+                m_dilated = None
+
+                if self.dilation is not None and self.dilation != 0:
+                    m_dilated = numpy.copy(m)
+                    m_dilated[scipy.ndimage.binary_dilation(m_dilated, iterations=self.dilation)] = 1
 
                 if self.mask_strategy == "xor":
-                    m = numpy.logical_xor(output[box[1]:box[3], box[0]:box[2]], m).astype(int)
+                    m = self._internal_xor(output, box, m)
                     m *= (i + 1)
                     output[box[1]:box[3], box[0]:box[2]] = m
+
+                    if m_dilated is not None:
+                        m_dilated = self._internal_xor(output_dilated, box, m_dilated)
+                        m_dilated *= (i + 1)
+                        output_dilated[box[1]:box[3], box[0]:box[2]] = m_dilated
                 elif self.mask_strategy == "addition":
                     m *= (i + 1)
                     output[box[1]:box[3], box[0]:box[2]] += m
-                elif self.mask_strategy == "ignore":
-                    m = m - output[box[1]:box[3], box[0]:box[2]]
-                    m[m <= 0] = 0
-                    m *= (i + 1)
-                    output[box[1]:box[3], box[0]:box[2]] = m
+
+                    if m_dilated is not None:
+                        m_dilated *= (i + 1)
+                        output_dilated[box[1]:box[3], box[0]:box[2]] += m_dilated
+                elif self.mask_strategy == "new xor":
+                    output[box[1]:box[3], box[0]:box[2]] = self._internal_new_xor(output, box, m, i)
+
+                    if m_dilated is not None:
+                        output_dilated[box[1]:box[3], box[0]:box[2]] = self._internal_new_xor(output_dilated, box,
+                                                                                              m_dilated, i)
+                elif self.mask_strategy == "ignore":  # ignores intersection of boxes to prevent bleedthough of masks
+                    output[box[1]:box[3], box[0]:box[2]] = self._internal_ignore(output, box, m) * (i + 1)
+
+                    if m_dilated is not None:
+                        n_box = [box[0] - dilation_extra, box[1] - dilation_extra, box[2] + dilation_extra, box[3] + dilation_extra]
+                        output_dilated[n_box[1]:n_box[3], n_box[0]:n_box[2]] = self._internal_new_xor(output_dilated, n_box,
+                                                                                             m_dilated, i)
         else:
             for i, box in tqdm.tqdm(enumerate(boxes), desc="Creating masks and thresholds", total=boxes.shape[0]):
                 m = self._generate_mask_from_scratch(
@@ -221,23 +271,43 @@ class MaskGenerator:
                         box[2] - box[0]
                     )
                 )
+                m_dilated = None
+
+                if self.dilation is not None and self.dilation != 0:
+                    m_dilated = numpy.copy(m)
+                    m_dilated = numpy.pad(m_dilated, [[dilation_extra, dilation_extra], [dilation_extra, dilation_extra]], mode="constant")
+                    m_dilated[scipy.ndimage.binary_dilation(m_dilated, iterations=self.dilation)] = 1
 
                 if self.mask_strategy == "xor":
-                    m = numpy.logical_xor(output[box[1]:box[3], box[0]:box[2]], m).astype(int)
+                    m = self._internal_xor(output, box, m)
                     m *= (i + 1)
                     output[box[1]:box[3], box[0]:box[2]] = m
+
+                    if m_dilated is not None:
+                        m_dilated = self._internal_xor(output_dilated, box, m_dilated)
+                        m_dilated *= (i + 1)
+                        output_dilated[box[1]:box[3], box[0]:box[2]] = m_dilated
                 elif self.mask_strategy == "addition":
                     m *= (i + 1)
                     output[box[1]:box[3], box[0]:box[2]] += m
-                elif self.mask_strategy == "new xor":
-                    temp = numpy.logical_xor(output[box[1]:box[3], box[0]:box[2]], m).astype(int)
-                    m1 = output[box[1]:box[3], box[0]:box[2]] * temp
-                    m1 += (numpy.logical_xor(m1, m) * (i + 1))
-                    output[box[1]:box[3], box[0]:box[2]] = m1
-                elif self.mask_strategy == "ignore":  # ignores intersection of boxes to prevent bleedthough of masks
-                    m = m - output[box[1]:box[3], box[0]:box[2]]
-                    m[m <= 0] = 0
-                    m *= (i + 1)
-                    output[box[1]:box[3], box[0]:box[2]] = m
 
-        return output
+                    if m_dilated is not None:
+                        m_dilated *= (i + 1)
+                        output_dilated[box[1]:box[3], box[0]:box[2]] += m_dilated
+                elif self.mask_strategy == "new xor":
+                    output[box[1]:box[3], box[0]:box[2]] = self._internal_new_xor(output, box, m, i)
+
+                    if m_dilated is not None:
+                        output_dilated[box[1]:box[3], box[0]:box[2]] = self._internal_new_xor(output_dilated, box,
+                                                                                              m_dilated, i)
+                elif self.mask_strategy == "ignore":  # ignores intersection of boxes to prevent bleedthough of masks
+                    output[box[1]:box[3], box[0]:box[2]] = self._internal_ignore(output, box, m) * (i + 1)
+
+                    if m_dilated is not None:
+                        n_box = [box[0] - dilation_extra, box[1] - dilation_extra, box[2] + dilation_extra,
+                                 box[3] + dilation_extra]
+                        output_dilated[n_box[1]:n_box[3], n_box[0]:n_box[2]] = self._internal_new_xor(output_dilated,
+                                                                                                      n_box,
+                                                                                                      m_dilated, i)
+
+        return output, output_dilated
